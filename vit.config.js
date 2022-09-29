deb4607508290c9b1c8a782046ac03739d6ac74f
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-09-29 16:02:39 +0530

    Update vit.config.js

--- vit.config.js
+++ vit.config.js
@@ -3,6 +3,5 @@ import {defineConfig} from ' vite '
 // https://vitejs.dev/config/
 export default defineConfig({
 
-    base: '/Chrome-Extension-01/website/'
-    // plugins : [vue()]
+    base: '/pbl2/'
 })
