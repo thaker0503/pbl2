4d93e87b97e4779bacf056555e1c20dfda0c4d03
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-09-29 15:59:54 +0530

    a

--- checkWebsite.js
+++ checkWebsite.js
@@ -13,83 +13,83 @@
 // const API_KEY = "7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6"
 
 
-// import axios from 'axios';
+import axios from 'axios';
 
 export function website(url) {
-  // const encodedParams = new URLSearchParams();
-  // encodedParams.set('apikey', '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6');
-  // encodedParams.set('url', url);
-  url = urlencode(url);
-  const key = "gK9daycB4hvyLu7b0iykkB1ARUasZDkm";
-  const options = {
-    method: 'POST',
-    url: `https://ipqualityscore.com/api/json/url/${key}/${url}`,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      // 'IPQS-KEY': 'gK9daycB4hvyLu7b0iykkB1ARUasZDkm',
-      'Access-Control-Allow-Origin': '*',
-    },
-    // data: encodedParams,
-  };
-
-  fetch(`https://ipqualityscore.com/api/json/url?key=gK9daycB4hvyLu7b0iykkB1ARUasZDkm&url=https%3A%2F%2Fgoogle.com`, {
-    method: "GET",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      // 'IPQS-KEY': 'gK9daycB4hvyLu7b0iykkB1ARUasZDkm',
-      'Access-Control-Allow-Origin': '*',
-      // "mode": "no-cors"
-    }
-  }).then(r => r.json()).then(data => console.log(data))
-
-
+  const encodedParams = new URLSearchParams();
+  encodedParams.set('apikey', '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6');
+  encodedParams.set('url', url);
+  // url = urlencode(url);
+  // const key = "gK9daycB4hvyLu7b0iykkB1ARUasZDkm";
   // const options = {
-  //   method: 'POST',
-  //   url: 'https://www.virustotal.com/api/v3/urls',
+  //   method: 'GET',
+  //   url: `https://ipqualityscore.com/api/json/url/${key}/${url}`,
   //   headers: {
   //     'Accept': 'application/json',
   //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'x-apikey': '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6',
+  //     'IPQS-KEY': 'gK9daycB4hvyLu7b0iykkB1ARUasZDkm',
   //     'Access-Control-Allow-Origin': '*',
   //   },
-  //   data: encodedParams,
+  //   // data: encodedParams,
   // };
 
-  // axios
-  //   .request(options)
-  //   .then(function (response) {
-  //     // getAnalysis(response.data.data.id);
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
+  // fetch(`https://ipqualityscore.com/api/json/url?key=gK9daycB4hvyLu7b0iykkB1ARUasZDkm&url=https%3A%2F%2Fgoogle.com`, {
+  //   method: "GET",
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //     // 'IPQS-KEY': 'gK9daycB4hvyLu7b0iykkB1ARUasZDkm',
+  //     'Access-Control-Allow-Origin': '*',
+  //     // "mode": "no-cors"
+  //   }
+  // }).then(r => r.json()).then(data => console.log(data))
+
+
+  const options = {
+    method: 'POST',
+    url: 'https://www.virustotal.com/api/v3/urls',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'x-apikey': '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6',
+      'Access-Control-Allow-Origin': '*',
+    },
+    data: encodedParams,
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      getAnalysis(response.data.data.id);
+      // console.log(response);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 
 }
 
-// function getAnalysis(id){
-//   const options = {
-//   method: 'GET',
-//   url: `https://www.virustotal.com/api/v3/analyses/${id}`,
-//   headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//       'x-apikey': '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6',
-//       'Access-Control-Allow-Origin': '*',
-//     }
-//   };
+function getAnalysis(id){
+  const options = {
+  method: 'GET',
+  url: `https://www.virustotal.com/api/v3/analyses/${id}`,
+  headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'x-apikey': '7aaa038e271ce99f15f498ba2a9bc34f0b926bd0244783cc7a0cb040aff53ae6',
+      'Access-Control-Allow-Origin': '*',
+    }
+  };
 
-//   axios
-//     .request(options)
-//     .then(function (response) {
-//       console.log("Response ==>" ,response.data.data.attributes.stats);
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
-// }
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log("Response ==>" ,response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
 
 
 
---
+++ vit.config.js
@@ -0,0 +1,8 @@
+import {defineConfig} from ' vite '
+// import vue from @vitejs / plugin - vue '
+// https://vitejs.dev/config/
+export default defineConfig({
+
+    base: '/Chrome-Extension-01/website/'
+    // plugins : [vue()]
+})
